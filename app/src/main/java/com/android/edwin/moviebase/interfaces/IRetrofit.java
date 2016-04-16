a06c67105f344b0ed7be79cdf793b9b934ff381f
Edwin Lambregts <edwin.lambregts0@gmail.com>
2016-04-16 12:47:36 +0200

    Edited GET path

--- app/src/main/java/com/android/edwin/moviebase/interfaces/IRetrofit.java
+++ app/src/main/java/com/android/edwin/moviebase/interfaces/IRetrofit.java
@@ -1,20 +1,17 @@
 package com.android.edwin.moviebase.interfaces;
 
 import com.android.edwin.moviebase.domain.Movie;
+import com.android.edwin.moviebase.util.Constants;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Path;
 
 /**
  * Created by Edwin on 2-4-2016.
  */
 public interface IRetrofit {
 
-    @GET("/movie/550")
-    Call<Movie> getMovie(String name);
-
-    @GET("/discover/movie?sort_by=popularity.desc")
+    @GET("/discover/movie?sort_by=popularity.desc" + Constants.API_PARAM)
     Call<List<Movie>> getMovies();
 
 }
