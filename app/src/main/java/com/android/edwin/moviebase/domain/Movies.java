5e9273d3884c05104bf716a3c31614e41fe9c154
Edwin Lambregts <edwin.lambregts0@gmail.com>
2016-04-17 12:03:08 +0200

    Added new Movies class

---
+++ app/src/main/java/com/android/edwin/moviebase/domain/Movies.java
@@ -0,0 +1,20 @@
+package com.android.edwin.moviebase.domain;
+
+import java.util.List;
+
+/**
+ * Created by lol_e on 16-4-2016.
+ */
+public class Movies {
+
+    List<Movie> movies;
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+}
