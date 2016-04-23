64b2757b6697f80e9166f09478d3c25060ecb090
Edwin Lambregts <edwin.lambregts0@gmail.com>
2016-04-23 17:38:45 +0200

    Finalized Movie class

--- app/src/main/java/com/android/edwin/moviebase/domain/Movie.java
+++ app/src/main/java/com/android/edwin/moviebase/domain/Movie.java
@@ -1,18 +1,42 @@
 package com.android.edwin.moviebase.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Edwin on 30-3-2016.
  */
 public class Movie {
 
-    private String title;
+    private String posterPath;
+    private Boolean adult;
     private String overview;
     private String release_date;
-    private int runtime;
+    private List<Integer> genreIds = new ArrayList<Integer>();
+    private Integer id;
+    private String originalTitle;
+    private String originalLanguage;
+    private String title;
+    private String backdropPath;
+    private Double popularity;
+    private Integer voteCount;
+    private Boolean video;
+    private Double voteAverage;
 
-    public Movie(String title, String overview) {
-        this.title = title;
-        this.overview = overview;
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
     }
 
     public String getOverview() {
@@ -23,12 +47,92 @@ public class Movie {
         this.overview = overview;
     }
 
+    public String getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(String release_date) {
+        this.release_date = release_date;
+    }
+
+    public List<Integer> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Integer> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
     public String getTitle() {
         return title;
     }
 
-    public void setSitle(String title) {
+    public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
     }
 
 }
