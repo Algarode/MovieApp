cccc846c27afda6b00b9a1b8b136e2da4384ca11
Edwin Lambregts <edwin.lambregts0@gmail.com>
2016-04-23 17:38:11 +0200

    Added standard path to get images

--- app/src/main/java/com/android/edwin/moviebase/util/Constants.java
+++ app/src/main/java/com/android/edwin/moviebase/util/Constants.java
@@ -9,5 +9,6 @@ public class Constants {
     public static final String API_KEY = "856de636e11c51b66e81ef1d76f18b05";
     public static final String API_PARAM = "?api_key=" + API_KEY;
     public static final String API_PARAM_AND = "&api_key=" + API_KEY;
+    public static final String IMAGE_PATH = "http://image.tmdb.org/t/p/w500";
 
 }
