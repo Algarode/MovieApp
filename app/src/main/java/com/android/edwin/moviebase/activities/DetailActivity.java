5242105fb1359f9ab64d9c09fd508e1703d95a03
Edwin Lambregts <edwin.lambregts0@gmail.com>
2016-07-25 22:16:35 +0200

    Added Collapsing Toolbar

--- app/src/main/java/com/android/edwin/moviebase/activities/DetailActivity.java
+++ app/src/main/java/com/android/edwin/moviebase/activities/DetailActivity.java
@@ -1,7 +1,26 @@
 package com.android.edwin.moviebase.activities;
 
-/**
- * Created by Edwin on 30-3-2016.
- */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.widget.Toolbar;
+import com.android.edwin.moviebase.R;
+
 public class DetailActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.MyToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.collapse_toolbar);
+        collapsingToolbarLayout.setTitle("My awesome title");
+        collapsingToolbarLayout.setCollapsedTitleTextColor(getResources().getColor(R.color.white));
+        collapsingToolbarLayout.setExpandedTitleColor(getResources().getColor(R.color.white));
+    }
+
 }
--- app/src/main/java/com/android/edwin/moviebase/activities/MainActivity.java
+++ app/src/main/java/com/android/edwin/moviebase/activities/MainActivity.java
@@ -1,10 +1,13 @@
 package com.android.edwin.moviebase.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import com.android.edwin.moviebase.R;
 import com.android.edwin.moviebase.adapters.ListAdapter;
@@ -23,8 +26,10 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
         listView = (ListView) findViewById(R.id.listView);
 
         BaseActivity baseActivity = new BaseActivity();
@@ -44,13 +49,19 @@ public class MainActivity extends BaseActivity {
             }
         });
 
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return false;
     }
 
     @Override
